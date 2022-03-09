5a222ae083509a6c0680f835297bb31cd94f7996
Fabio Vilas <fabiovilaas@gmail.com>
2022-03-09 11:17:24 -0300

    adiciona exercicio 3 e 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,4 +1,4 @@
-//Exercicio 1 - faça programa para cada operação aritmética
+//Exercicio 1
 const a = 5;
 const b = 2;
 
@@ -10,8 +10,7 @@ let modulo = a % b;
 
 console.log(modulo);
 
-//Exercicio 2 - Retorne o maior de dois numeros
-
+//Exercicio 2 
 const num1 = 10;
 const num2 = 30;
 
@@ -22,3 +21,28 @@ else {
     console.log(num2);
 }
 
+//Ex 3
+const numero1 = 3;
+const numero2 = 30;
+const numero3 = 12;
+
+if(numero1 > numero2 && numero1 > numero3){
+    console.log(numero1);
+}
+else if(numero2 > numero1 && numero2 > numero3){
+    console.log(numero2);
+}
+else{
+    console.log(numero3);
+}
+
+//Ex 4
+const n = -1;
+
+if(n > 0) {
+        console.log("Positivo")
+} else if(n < 0) {
+        console.log("Negativo")
+} else {
+            console.log("Zero")
+}
