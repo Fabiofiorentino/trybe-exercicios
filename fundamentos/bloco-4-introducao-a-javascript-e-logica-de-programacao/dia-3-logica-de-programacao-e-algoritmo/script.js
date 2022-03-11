b3236f01e9b2f63435f41cc7759536dc432981f1
Fabio Vilas <fabiovilaas@gmail.com>
2022-03-11 11:07:55 -0300

    adiciona exercicio 4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmo/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmo/script.js
@@ -39,3 +39,19 @@ for (let index = 0; index < array.length; index += 1) {
 console.log(bigWord);
 console.log(smallWord);
 
+//4 copiado
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 0; currentNumber <= 50; currentNumber += 1) {
+  let isPrime = true;
+  for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+    if (currentNumber % currentDivisor === 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    biggestPrimeNumber = currentNumber;
+  }
+}
+
+console.log(biggestPrimeNumber);
