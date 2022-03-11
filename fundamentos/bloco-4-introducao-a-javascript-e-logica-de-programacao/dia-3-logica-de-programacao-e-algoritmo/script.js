15dad7a8d6aee4741d264c8b6f8e3dc94760ce6b
Fabio Vilas <fabiovilaas@gmail.com>
2022-03-11 10:42:40 -0300

    adiciona exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmo/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-logica-de-programacao-e-algoritmo/script.js
@@ -17,3 +17,25 @@ for (let i = 0; i < word.length; i += 1) {
 }
 
 console.log(reverseWord);
+
+//3
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let bigWord = array[0];
+let smallWord = array[0];
+
+for (let i = 0; i < array.length; i += 1) {
+  if (array[i].length > bigWord.length) {
+    bigWord = array[i];
+  }
+}
+
+for (let index = 0; index < array.length; index += 1) {
+  if (array[index].length < smallWord.length) {
+    smallWord = array[index];
+  }
+}
+
+console.log(bigWord);
+console.log(smallWord);
+
