f47548ea09c9e1ad844eb2bc3e45925bc827d298
Fabio Vilas <fabiovilaas@gmail.com>
2022-03-27 14:37:02 -0300

    exercicio 4

---
+++ fundamentos/bloco-06-HTML-e-CSS-forms-flexbox-e-responsivos/dia-1-HTML-e-CSS-forms/script.js
@@ -0,0 +1,7 @@
+const submmitButton = document.getElementById('enviar')
+function interrompeButton (event){
+
+    event.preventDefault();
+}
+
+submmitButton.addEventListener('click', interrompeButton);
